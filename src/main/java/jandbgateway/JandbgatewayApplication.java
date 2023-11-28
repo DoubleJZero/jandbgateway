9735c5d3f307f2907b09ce59d5f458955d97a47e
JandB <blueseajung@naver.com>
2023-11-28 14:34:13 +0900

    first commit

---
+++ src/main/java/jandbgateway/JandbgatewayApplication.java
@@ -0,0 +1,15 @@
+package jandbgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JandbgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JandbgatewayApplication.class, args);
+	}
+
+}
